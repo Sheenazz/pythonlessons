49e42062d7019fb1dc20c61e2bd06270169de7a2
Ada <ada.skulska@gmail.com>
2018-07-14 14:00:31 +0200

    Return largest numers in array lesson - fix

--- return-largest-numbers-in-arrays/lessons.py
+++ return-largest-numbers-in-arrays/lessons.py
@@ -1,12 +1,3 @@
-def convertFahrenheitsToCelsius(fahrenheit):
-  return fahrenheit-32 * 5.0 / 9.0
-
-Fahrenheit = int(input("Enter..."))
-
-Celsius = convertFahrenheitsToCelsius(Fahrenheit)
-
-print(Fahrenheit, "Fahrenheit", Celsius, "Celsius")
-
 def reverseString(string):
   print("Reverse string", string[::-1])
 
