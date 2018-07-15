98d63a5f7d7a7ed721d1f9509ee84cce314072cb
Ada <ada.skulska@gmail.com>
2018-07-15 14:25:05 +0200

    Return largest number in arrays

--- return-largest-numbers-in-arrays/lessons.py
+++ return-largest-numbers-in-arrays/lessons.py
@@ -1,5 +1,16 @@
-def reverseString(string):
-  print("Reverse string", string[::-1])
+numbers = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]
 
-reverseString("test")
-# print("Reverse string", reverseString("test"))
+arrayOfLargestNumber = []
+def findingLargestNumberInArrays(arrayOfArraysOfNumbers):
+    for arrayOfNumbers in arrayOfArraysOfNumbers:
+      max = arrayOfNumbers[0]
+      for number in arrayOfNumbers:
+        if number > max:
+            max = number
+      arrayOfLargestNumber.append(max)
+
+findingLargestNumberInArrays(numbers)
+
+print("Largest found:")
+for number in arrayOfLargestNumber:
+    print(number)
