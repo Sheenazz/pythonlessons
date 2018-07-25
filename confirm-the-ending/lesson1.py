db3d9e93cc732fb44aba67111d141030daa2d631
Ada <ada.skulska@gmail.com>
2018-07-25 17:15:46 +0200

    finders-keepers

---
+++ confirm-the-ending/lesson1.py
@@ -0,0 +1,7 @@
+str = "Never give up and good luck will find you"
+
+suffix = "you"
+array = str.split()
+GoodEnding = len(array)
+if array[GoodEnding-1] == suffix:
+    print("True")
---
+++ finders-keepers/lessons.py
@@ -0,0 +1,7 @@
+sentence = "I'm a little tea pot"
+array = [" "]
+def CapitalizeFirstLetter(array):
+    if array = [0]:
+        array.capitalize()
+
+print(CapitalizeFirstLetter)
