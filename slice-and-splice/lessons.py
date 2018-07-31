c555fbdc8a8725bcc123ff8660bbd334aee28452
yerevin <skulskimat@gmail.com>
2018-07-31 21:52:32 +0200

    Update

--- slice-and-splice/lessons.py
+++ slice-and-splice/lessons.py
@@ -1,9 +1,13 @@
+def copiedArray(firstArray, secondArray, toInsertIndex):
+    print(firstArray[1:toInsertIndex])
+    firstArray[toInsertIndex:toInsertIndex] = secondArray
+    return firstArray
+    # for index, secondArrayElement in enumerate(secondArray):
+    #     firstArray.insert(toInsertIndex+index, secondArrayElement)
+    # return firstArray
+
 firstArray = [1, 2, 3]
 secondArray = [4, 7, 8]
+index = 2   
 
-def copiedArray(firstArray, secondArray, index):
-    index = [1]
-    firstArray[2] = secondArray
-    return firstArray
-
-print(firstArray(index))
+print(copiedArray(firstArray, secondArray, index))
--- truncate-a-string/lessons.py
+++ truncate-a-string/lessons.py
@@ -1,8 +1,10 @@
 def theLongestString(passedSentence, length):
-    wordsArray = passedSentence.split()
-    if len(passedSentence) >= 8:
-        return wordsArray[0]
+    if len(passedSentence) > length:
+        return passedSentence[:length] + '...'
+    else:
+        return passedSentence
+
 
 sentence = "A-tisket a-tasket A green and yellow basket"
 
-print(theLongestString(sentence, 8),"...")
+print(theLongestString(sentence, 12))
